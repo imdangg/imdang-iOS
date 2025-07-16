4320c4ea55bc8badd97d6a026bece431c0b7df34
DAEJINLIM <daejin.limm@gmail.com>
2025-07-16 22:40:38 +0900

    Fix: 삭제 실수 복구

--- Projects/imdang/Sources/Common/TextField/TextFieldHeaderView.swift
+++ Projects/imdang/Sources/Common/TextField/TextFieldHeaderView.swift
@@ -15,12 +15,19 @@ import Then
 class TextFieldHeaderView: UIView {
     
     let title: String
+    let isEssential: Bool
     let descriptionText: String?
     var limitNumber: Int?
     
     var titleLabel = UILabel().then {
         $0.font = .pretenMedium(14)
         $0.textColor = UIColor.grayScale600
+    }
+    
+    private var EssentialLabel = UILabel().then {
+        $0.font = .pretenMedium(14)
+        $0.text = "*"
+        $0.textColor = UIColor.error
     }
     
     private var signImage = UIImageView()
@@ -37,6 +44,7 @@ class TextFieldHeaderView: UIView {
     
     init(frame: CGRect = .zero, title: String, isEssential: Bool, descriptionText: String? = nil, limitNumber: Int? = nil) {
         self.title = title
+        self.isEssential = isEssential
         self.descriptionText = descriptionText
         self.limitNumber = limitNumber
         super.init(frame: frame)
@@ -55,7 +63,7 @@ class TextFieldHeaderView: UIView {
     }
     
     private func addSubViews() {
-        [titleLabel, signImage, descriptionLabel, textNumLabel].forEach { addSubview($0) }
+        [titleLabel, EssentialLabel, signImage, descriptionLabel, textNumLabel].forEach { addSubview($0) }
     }
     
     private func makeUI() {
@@ -63,9 +71,20 @@ class TextFieldHeaderView: UIView {
             $0.leading.equalToSuperview()
         }
         
-        signImage.snp.makeConstraints {
-            $0.leading.equalTo(titleLabel.snp.trailing).offset(6)
-            $0.centerY.equalTo(titleLabel)
+        if isEssential {
+            EssentialLabel.snp.makeConstraints {
+                $0.leading.equalTo(titleLabel.snp.trailing)
+                $0.centerY.equalTo(titleLabel)
+            }
+            signImage.snp.makeConstraints {
+                $0.leading.equalTo(EssentialLabel.snp.trailing).offset(4)
+                $0.centerY.equalTo(titleLabel)
+            }
+        } else {
+            signImage.snp.makeConstraints {
+                $0.leading.equalTo(titleLabel.snp.trailing).offset(6)
+                $0.centerY.equalTo(titleLabel)
+            }
         }
     
         textNumLabel.snp.makeConstraints {
